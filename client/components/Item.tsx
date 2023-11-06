2f9f1c0389b490952890b8e1b7b642f50aec8d40
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2023-11-05 21:52:41 -0500

    Revert "delete directory"
    This reverts commit 29948e33cec8d16d66790800ccd1f9b8fc5203ee.

---
+++ client/components/Item.tsx
@@ -0,0 +1,51 @@
+import { Text, View, Button } from "react-native"
+
+const months = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
+const Item = (name: String, exp: Date, handler: ()=>void) => {
+    return (
+        <View 
+            style={{
+                width: '100%',
+                height: 64,
+                backgroundColor: 'lightgray',
+                borderRadius: 10,
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                padding: 16,
+            }}
+        >
+            <Text
+                style={{
+                    height: '100%',
+                    textAlignVertical: 'center',
+                    fontSize: 20,
+                }}
+            >
+                {name}
+            </Text>
+            <View style={{flexGrow: 1,}}></View>
+            <Text
+                style={{
+                    height: '100%',
+                    textAlignVertical: 'center',
+                    fontSize: 20,
+                    paddingRight: 2,
+                }}
+            >
+                {months[Number(exp.getMonth().toString())]}
+                {' '}
+                {exp.getDay().toString()}
+            </Text>
+            <Button
+                title="X"
+                onPress={handler}
+            />
+        </View>
+    )
+}
+
+export default Item
